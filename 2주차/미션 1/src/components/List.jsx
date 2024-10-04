ac381d3e91d3ab4d9663ce9685b51a338c214d62
witty._0 <mercury0502@dgu.ac.kr>
2024-10-04 23:48:25 +0900

    Delete 2주차/미션 1/src directory

--- 2주차/미션 1/src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-const List = (props) => {
-    const { tech, food, yaho }=props;
-    return (
-        <li style={{listStyle: 'none'}}>
-            {tech}
-        </li>
-    )
-}
-export default List
