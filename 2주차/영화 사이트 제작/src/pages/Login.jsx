e09684c4fd6b60980b7a463fa7af0a0a94ee125d
lwittyl <mercury0502@dgu.ac.kr>
2024-10-10 19:32:15 +0900

    3주차 mission 완료

---
+++ 2주차/영화 사이트 제작/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+const Login = () => {
+    return (
+        <LogIn><h1>로그인 페이지</h1></LogIn>
+    );
+}
+
+export default Login;
+
+const LogIn = styled.div`
+    width: 90%;
+    padding: 1rem 2rem;
+    color:white;
+`
---
+++ 2주차/영화 사이트 제작/src/pages/SignUp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+const SignUp = () => {
+    return (
+        <Signup><h1>회원가입 페이지</h1></Signup>
+    );
+}
+
+export default SignUp;
+
+const Signup = styled.div`
+    width: 90%;
+    padding: 1rem 2rem;
+    color:white;
+`
