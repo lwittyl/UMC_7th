cdbeda226d688ec02bef647347fe0ba1b83df642
lwittyl <mercury0502@dgu.ac.kr>
2024-11-03 23:20:24 +0900

    5주차 - mission

--- 3~5주차/영화 사이트 제작/src/pages/SignUp.jsx
+++ 3~5주차/영화 사이트 제작/src/pages/SignUp.jsx
@@ -4,8 +4,19 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 const SignUp = () => {
     const schema = yup.object().shape({
-        email: yup.string().email("이메일을 정확히 입력해 주세요."),
-        password: yup.string().min(8, '비밀번호는 8자 이상이어야 합니다.').max(16, '비밀번호는 16자 이하여야 합니다.').required(),
+        email: yup
+            .string()
+            .email("이메일을 정확히 입력해 주세요.")
+            .required('이메일을 반드시 입력해주세요'),
+        password: yup
+            .string()
+            .min(8, '비밀번호는 8자 이상이어야 합니다')
+            .max(16, '비밀번호는 16자 이하여야 합니다')
+            .required('비밀번호를 반드시 입력해주세요'),
+        passwordCheck: yup
+            .string()
+            .oneOf([yup.ref('password'), null], '비밀번호가 일치하지 않습니다.')
+            .required('비밀번호 검증 또한 필수 입력요소입니다'),
     })
     const {register, handleSubmit, formState: {errors, isValid}} = useForm({
         resolver: yupResolver(schema),
@@ -17,13 +28,15 @@ const SignUp = () => {
     return (
         <Signup>
             <SingnupContainer>
-                <h1>로그인</h1>
+                <h1>회원가입</h1>
                 <Form onSubmit={handleSubmit(onSubmit)}>
                     <Input type={'email'} {...register("email")} placeholder='이메일을 입력해주세요!'/>
                     <p style={{color: 'red'}}>{errors.email?.message}</p>
                     <Input type={'password'} {...register("password")} placeholder='비밀번호를 입력해주세요!'/>
                     <p style={{color: 'red'}}>{errors.password?.message}</p>
-                    <Submit type={'submit'} value='로그인' isValid={isValid}/>
+                    <Input type={'password'} {...register("passwordCheck")} placeholder='비밀번호를 다시 입력해주세요!'/>
+                    <p style={{color: 'red'}}>{errors.passwordCheck?.message}</p>
+                    <Submit type={'submit'} value='제출' isValid={isValid}/>
                 </Form>
             </SingnupContainer>
         </Signup>
